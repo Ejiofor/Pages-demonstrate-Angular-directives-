779e1e08d3fb59e1cbc23ce4b87dcadf17f9f17d
Ejiofor <nwofor@ymail.com>
2020-11-06 05:18:26 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  currentPage = 1;
+  images = [
+    {
+      title: 'by the beach side',
+      url: 'https://unsplash.com/photos/5brvJbR1Pn8'
+    },
+    {
+      title: 'by the beach side',
+      url: 'https://unsplash.com/photos/5brvJbR1Pn8'
+    },
+    {
+      title: 'by the beach side',
+      url: 'https://unsplash.com/photos/5brvJbR1Pn8'
+    },
+    {
+      title: 'by the beach side',
+      url: 'https://unsplash.com/photos/5brvJbR1Pn8'
+    },
+    {
+      title: 'by the beach side',
+      url: 'https://unsplash.com/photos/5brvJbR1Pn8'
+    },
+    {
+      title: 'by the beach side',
+      url: 'https://unsplash.com/photos/5brvJbR1Pn8'
+    },
+    {
+      title: 'by the beach side',
+      url: 'https://unsplash.com/photos/5brvJbR1Pn8'
+    },
+    {
+      title: 'by the beach side',
+      url: 'https://unsplash.com/photos/5brvJbR1Pn8'
+    },
+    {
+      title: 'by the beach side',
+      url: 'https://unsplash.com/photos/5brvJbR1Pn8'
+    },
+    {
+      title: 'by the restaurant side',
+      url: 'https://unsplash.com/photos/nmpW_WwwVSc'
+    },
+    {
+      title: 'by the garden side',
+      url: 'https://unsplash.com/photos/bYaqZ8WdoVA'
+    }
+  ];
 }
